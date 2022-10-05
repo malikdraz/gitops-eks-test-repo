ab5f84447fe093a5ac6a9af1281345aafe400efc
Malik Draz <malikdraz@hotmail.com>
2022-10-05 20:48:06 +1100

    updating file

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Kyra Louise Jordan, welcome to malik office!'
+    return 'Hello World, welcome to Malik GitOps pipeline!'
