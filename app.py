2aced3322acec8b7163a900a5c816d87bcdedeb0
Malik Draz <malikdraz@hotmail.com>
2022-10-05 21:34:29 +1100

    updated file

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World, my name is Ali Draz!'
+    return 'Thi is a GitOps pipeline with Jenkins and Amazon EKS cluster!'
